2c86eef7dc5980c45e3476ba6b40e6a10a1ba6f8
Роман Вятчанин <viat4anin@gmail.com>
2022-11-26 17:12:19 +0300

    Homework 1

---
+++ HellowPython/lec.py
@@ -0,0 +1,11 @@
+print('hello world')
+a = 123
+b = 1.23
+print(a,b)
+s = "hellow world" #работает и наоборот
+print(type(a)) #вывод типа
+print('{} - {} - {}'.format(a,b,s))
+print(f'{a} - {b} - {s}')
+print('{1} - {2} - {0}'.format(a,b,s))
+list = [1,2,3,1,'123']
+print(list)
